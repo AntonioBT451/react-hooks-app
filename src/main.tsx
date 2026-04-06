4f06b5b120e9b23b5f4488b07d2b130e090ab8bd
AntonioBT451 <antoniobt451@gmail.com>
2026-04-06 08:27:21 -0600

    feat: add Scramble Words game with useState and confetti effects

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,11 @@ import './index.css'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 // import { PokemonPage } from './03-examples/PokemonPage'
 // import { FocusScreen } from './04-useRef/FocusScreen'
-import { TaskApp } from './05-useReducer/TaskApp'
+// import { TaskApp } from './05-useReducer/TaskApp'
+import { ScrambleWords } from './05-useReducer/ScrambleWords'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TaskApp />
+    <ScrambleWords />
   </StrictMode>,
 )
