56a727d6d116de473c8e6cc8e8e63fb20dc17d90
AntonioBT451 <antoniobt451@gmail.com>
2026-04-09 18:58:58 -0600

    feat: add useCallback to prevent re-renders of memoized MySubTitle component

--- src/06-memos/MemoHook.tsx
+++ src/06-memos/MemoHook.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { MyTitle } from './ui/MyTitle';
 import { MySubTitle } from './ui/MySubTitle';
 
@@ -7,12 +7,16 @@ export const MemoHook = () => {
     const [title, setTitle] = useState('Hola');
     const [subTitle, setSubTitle] = useState('Mundo');
 
+    const handleMyApiCall = useCallback(() => {
+        console.log('Lamar API', subTitle);
+    }, [subTitle]);
+
     return (
         <div className="bg-gradient flex flex-col gap-4">
             <h1 className="text-2xl font-thin text-white">MemoHook</h1>
 
             <MyTitle title={title} />
-            <MySubTitle subTitle={subTitle} />
+            <MySubTitle subTitle={subTitle} callMyApi={handleMyApiCall} />
 
             <button
                 className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
--- src/06-memos/ui/MySubTitle.tsx
+++ src/06-memos/ui/MySubTitle.tsx
@@ -2,15 +2,19 @@ import { memo } from "react";
 
 interface Props {
     subTitle: string;
+    callMyApi: () => void;
 }
 
-export const MySubTitle = memo(({ subTitle }: Props) => {
+export const MySubTitle = memo(({ subTitle, callMyApi }: Props) => {
     console.log('Mi Sub-title re-render');
 
     return (
         <>
             <h6 className="text-xl font-bold">{subTitle}</h6>
-            <button className="bg-indigo-500 text-white px-2 py-2 rounded-md cursor-pointer">
+            <button
+                className="bg-indigo-500 text-white px-2 py-2 rounded-md cursor-pointer"
+                onClick={callMyApi}
+            >
                 Llamar a función
             </button>
         </>
