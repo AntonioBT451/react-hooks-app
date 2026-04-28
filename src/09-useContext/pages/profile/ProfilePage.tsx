7c9170e68402325c69352491ba4a3a7c1947d3d3
AntonioBT451 <antoniobt451@gmail.com>
2026-04-28 09:25:23 -0600

    feat: implement ProfilePage with user data display and logout functionality

--- src/09-useContext/pages/profile/ProfilePage.tsx
+++ src/09-useContext/pages/profile/ProfilePage.tsx
@@ -1,6 +1,31 @@
+import { UserContext } from "@/09-useContext/context/UserContext";
+import { Button } from "@/components/ui/button";
+import { useContext } from "react";
+import { useNavigate } from "react-router";
 
 export const ProfilePage = () => {
+
+    const { user, logout } = useContext(UserContext);
+    const navigation = useNavigate();
+
+    const handleSalir = () => {
+        logout();
+        navigation('/login');
+    }
+
     return (
-        <div>ProfilePage</div>
+        <div className="flex flex-col items-center justify-center min-h-screen min-w-screen">
+            <h1 className="text-4xl font-thin">Perfil del usuario: {user?.name}</h1>
+            <hr />
+
+            <pre className="my-4 w-2/3 overflow-x-auto">{JSON.stringify({ user }, null, 2)}</pre>
+
+            <Button
+                variant={'destructive'}
+                onClick={() => handleSalir()}
+            >
+                Salir
+            </Button>
+        </div>
     )
 };
