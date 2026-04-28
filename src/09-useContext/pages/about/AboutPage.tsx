2a2f7745e127e3dc37b810276ef9c30aa59346be
AntonioBT451 <antoniobt451@gmail.com>
2026-04-28 09:22:09 -0600

    feat: add authentication to AboutPage

--- src/09-useContext/pages/about/AboutPage.tsx
+++ src/09-useContext/pages/about/AboutPage.tsx
@@ -1,14 +1,31 @@
+import { UserContext } from "@/09-useContext/context/UserContext";
+import { Button } from "@/components/ui/button";
+import { use } from "react";
 import { Link } from "react-router";
 
 export const AboutPage = () => {
+
+    const { isAuthenticated, logout } = use(UserContext);
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen gap-2">
             <h1 className="text-4xl font-thin">Página sobre mi</h1>
             <hr />
 
             <div className="flex flex-col gap-2">
-                <Link to={"/profile"} className="underline text-xl hover:text-blue-600">Perfil</Link>
-                <Link to={"/login"} className="underline text-xl hover:text-blue-600">Login</Link>
+                {
+                    isAuthenticated ? (
+                        <>
+                            <Link to={"/profile"} className="underline text-xl hover:text-blue-600">Perfil</Link>
+                            <Button variant={'destructive'} className="mt-4"
+                                onClick={logout}>
+                                Salir
+                            </Button>
+                        </>
+                    ) : (
+                        <Link to={"/login"} className="underline text-xl hover:text-blue-600">Iniciar sesión</Link>
+                    )
+                }
             </div>
         </div>
     )
