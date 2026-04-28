c6483e688270b795d73b68a0fa2e221d8f6da2e5
AntonioBT451 <antoniobt451@gmail.com>
2026-04-28 09:24:07 -0600

    feat: add authentication logic to LoginPage

--- src/09-useContext/pages/auth/LoginPage.tsx
+++ src/09-useContext/pages/auth/LoginPage.tsx
@@ -1,8 +1,30 @@
+import { UserContext } from "@/09-useContext/context/UserContext";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Link } from "react-router";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router";
+import { toast } from "sonner";
 
 export const LoginPage = () => {
+
+    const { login } = useContext(UserContext);
+    const [userId, setUserId] = useState('');
+
+    const navigation = useNavigate();
+
+    const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const result = login(Number(userId));
+
+        if (!result) {
+            toast.error('Usuario no encontrado');
+            return;
+        }
+
+        navigation('/profile');
+    }
+
     return (
         <div className="flex flex-col items-center justify-center max-h-screen">
             <div className="flex flex-col items-center bg-white rounded-lg w-full max-w-md p-6 gap-8">
@@ -11,7 +33,10 @@ export const LoginPage = () => {
                     <div className="w-full h-px bg-slate-400" />
                 </div>
 
-                <form className="flex flex-col gap-3">
+                <form
+                    className="flex flex-col gap-3"
+                    onSubmit={(event) => handleSubmit(event)}
+                >
                     <div className="space-y-1">
                         <label htmlFor="userId" className="text-sm font-medium text-slate-900">Usuario</label>
                         <Input
@@ -23,6 +48,8 @@ export const LoginPage = () => {
                                 ring-1 ring-slate-400 
                                 hover:ring-1 hover:ring-blue-600
                                 focus-visible:ring-1 focus-visible:ring-blue-800"
+                            value={userId}
+                            onChange={(event) => setUserId(event.target.value)}
                         />
                     </div>
                     <Button type="submit" className="bg-blue-800 hover:bg-blue-900 text-white transition-colors">Login</Button>
